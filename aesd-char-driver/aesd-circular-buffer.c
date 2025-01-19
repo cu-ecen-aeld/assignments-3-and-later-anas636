ef8ef17dc523725eddf3381197a901b10a4e0a18
anas <you@example.com>
2025-01-19 16:43:29 +0100

    Assignment 7 Part 1

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -15,7 +15,7 @@
 #endif
 
 #include "aesd-circular-buffer.h"
-
+//#include <stdio.h>
 /**
  * @param buffer the buffer to search for corresponding offset.  Any necessary locking must be performed by caller.
  * @param char_offset the position to search for in the buffer list, describing the zero referenced
@@ -32,7 +32,43 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     /**
     * TODO: implement per description
     */
-    return NULL;
+    uint8_t out_off_;
+    uint8_t i = 0;
+    size_t index = 0;
+    out_off_ = buffer->out_offs;
+    
+    while (index < char_offset){
+
+        if(buffer->entry[out_off_].size == 0)
+            return NULL;
+        while(i < buffer->entry[out_off_].size){  
+            if (index == char_offset)
+                break;
+            if (i ==  buffer->entry[out_off_].size -1){
+                i=0;
+                index++;
+                out_off_++;
+                if (out_off_ >= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED){
+                    out_off_ = 0;
+                }
+                if (out_off_ == buffer->out_offs){
+                    return NULL;
+                }
+                break;
+            }
+            else{
+           
+                index++;
+                i++;
+            }
+        }
+
+    }
+    //printf("la valeur final de i est : %d\n", i);
+    //printf("la valeur final de index est : %ld \n", index);
+
+    *entry_offset_byte_rtn = i;
+    return &(buffer->entry[out_off_]) ;
 }
 
 /**
@@ -47,6 +83,23 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description
     */
+    //printf(" \n******************boool value is %d \n", buffer->full);
+
+    buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
+    buffer->entry[buffer->in_offs].size = add_entry->size;
+    (buffer->in_offs)++;
+
+    if (buffer->full){
+        (buffer->out_offs)++;
+        if(buffer->out_offs >= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+            buffer->out_offs = 0;
+    }
+
+    if((buffer->in_offs) >= AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED){
+        buffer->full = true;
+        buffer->in_offs = 0;
+    }   
+
 }
 
 /**
